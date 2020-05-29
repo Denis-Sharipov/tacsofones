fde528160884b3ce6775593e2befa331ee0ac940
Denis-Sharipov <50290543+Denis-Sharipov@users.noreply.github.com>
2020-05-29 07:24:34 +0300

    Add files via upload

---
+++ lab2/MoscowTransport.WebService/ApplicationServices/GetTacsofonObjListUseCase/GetTacsofonObjListUseCase.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using TacsofonObj.DomainObjects;
+using TacsofonObj.DomainObjects.Ports;
+using TacsofonObj.ApplicationServices.Ports;
+
+namespace TacsofonObj.ApplicationServices.GetTacsofonObjListUseCase
+{
+    public class GetTacsofonObjListUseCase : IGetTacsofonObjListUseCase
+    {
+        private readonly IReadOnlyTacsofonObjRepository _readOnlyTacsofonObjRepository;
+
+        public GetTacsofonObjListUseCase(IReadOnlyTacsofonObjRepository readOnlyTacsofonObjRepository) 
+            => _readOnlyTacsofonObjRepository = readOnlyTacsofonObjRepository;
+
+        public async Task<bool> Handle(GetTacsofonObjListUseCaseRequest request, IOutputPort<GetTacsofonObjListUseCaseResponse> outputPort)
+        {
+            IEnumerable<DomainObjects.TacsofonObj> tacsofonObjs = null;
+            if (request.TacsofonObjId != null)
+            {
+                var tacsofonObj = await _readOnlyTacsofonObjRepository.GetTacsofonObj(request.TacsofonObjId.Value);
+                tacsofonObjs = (tacsofonObj != null) ? new List<DomainObjects.TacsofonObj>() { tacsofonObj } : new List<DomainObjects.TacsofonObj>();
+                
+            }
+            else if (request.Platnost != null)
+            {
+                tacsofonObjs = await _readOnlyTacsofonObjRepository.QueryTacsofonObj(new PlatnostCriteria(request.Platnost));
+            }
+            else
+            {
+                tacsofonObjs = await _readOnlyTacsofonObjRepository.GetAllTacsofonObjs();
+            }
+            outputPort.Handle(new GetTacsofonObjListUseCaseResponse(tacsofonObjs));
+            return true;
+        }
+    }
+}
---
+++ lab2/MoscowTransport.WebService/ApplicationServices/Repositories/DbTacsofonObjRepository.cs
@@ -0,0 +1,37 @@
+﻿using TacsofonObj.ApplicationServices.Ports.Gateways.Database;
+using TacsofonObj.DomainObjects;
+using TacsofonObj.DomainObjects.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace TacsofonObj.ApplicationServices.Repositories
+{
+    public class DbTacsofonObjRepository : IReadOnlyTacsofonObjRepository,
+                                     ITacsofonObjRepository
+    {
+        private readonly ITacsofonObjDatabaseGateway _databaseGateway;
+
+        public DbTacsofonObjRepository(ITacsofonObjDatabaseGateway databaseGateway)
+            => _databaseGateway = databaseGateway;
+
+        public async Task<DomainObjects.TacsofonObj> GetTacsofonObj(long id)
+            => await _databaseGateway.GetTacsofonObj(id);
+
+        public async Task<IEnumerable<DomainObjects.TacsofonObj>> GetAllTacsofonObjs()
+            => await _databaseGateway.GetAllTacsofonObj();
+
+        public async Task<IEnumerable<DomainObjects.TacsofonObj>> QueryTacsofonObj(ICriteria<DomainObjects.TacsofonObj> criteria)
+            => await _databaseGateway.QueryTacsofonObj(criteria.Filter);
+
+        public async Task AddTacsofonObj(DomainObjects.TacsofonObj tacsofonObj)
+            => await _databaseGateway.AddTacsofonObj(tacsofonObj);
+
+        public async Task RemoveTacsofonObj(DomainObjects.TacsofonObj tacsofonObj)
+            => await _databaseGateway.RemoveTacsofonObj(tacsofonObj);
+
+        public async Task UpdateTacsofonObj(DomainObjects.TacsofonObj tacsofonObj)
+            => await _databaseGateway.UpdateTacsofonObj(tacsofonObj);
+    }
+}
